3179a456b2507d744d8bddad851784b90d37dbf6
Roffelos <andreacciolyl@hotmail.com>
2013-09-27 16:06:58 -0300

    Adicionado de verdade o Name

--- macros_pt_br.py
+++ macros_pt_br.py
@@ -1,3 +1,7 @@
 # coding: utf-8
 
-EXCEPTION_000 = "A senha deve conter no mínimo 6 caracteres"
+EXCEPTION_000 = "A senha deve conter no mínimo 6 caracteres."
+EXCEPTION_001 = "O nome deve conter menos de 32 caracteres."
+EXCEPTION_002 = "O nome não pode ser nulo."
+EXCEPTION_003 = "O nome deve conter apenas caracteres alphanuméricos."
+
