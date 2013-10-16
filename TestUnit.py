88cd1e6eca6a09675ca13adc3c981674d57d0317
Yurickh <yurick.hauschild@gmail.com>
2013-10-16 15:37:44 -0300

    me metendo lol

--- TestUnit.py
+++ TestUnit.py
@@ -1,4 +1,5 @@
 #coding: utf-8
+import inspect
 
 class TestUnit:
 	
@@ -13,9 +14,15 @@ class TestUnit:
 
 		classType = globals()[className]
 		attrList = filter(foo, dir(classType))
+
+		arguments = inspect.getargspec(classType.__init__).args
+		arguments.pop(0)
+
+		for elem in arguments:
+			elem = testValues[elem]
 		
 		try:
-			testing = classType(*testValues)
+			testing = classType(*arguments)
 			print "No errors ocurred in the object's creation process."
 			for attrStr, value in zip(attrList, testValues):
 				attr = getattr(testing, attrStr)
