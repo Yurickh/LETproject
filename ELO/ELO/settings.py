1979ea3acd781045c638a07b98a7b87fb0e51890
Yurickh <yurick.hauschild@gmail.com>
2013-10-30 14:32:57 -0200

    Adicionada uma pasta de database e o settings.py foi modificado para suportar o sqlite3

--- ELO/ELO/settings.py
+++ ELO/ELO/settings.py
@@ -13,13 +13,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(os.path.dirname(__file__), 'database/elo.db').replace('\\','/'),
         'USER': '',
         'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'HOST': '',
+        'PORT': '',
     }
 }
 
