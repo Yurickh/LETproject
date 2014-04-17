8bdda3ed3ddf121dee0c9c520beca484b9d66b56
Yurick <yurick.hauschild@gmail.com>
2014-04-17 16:57:05 -0300

    Editado ProfileUnit.py para se adaptar ao padrão de 80 caracteres.

--- ELO/Profile/ProfileUnit.py
+++ ELO/Profile/ProfileUnit.py
@@ -8,13 +8,16 @@ from django.shortcuts import render
 from django import forms
 
 ## @package ProfileUnit
-# Este arquivo é responsável pelo armazenamento de todas as camadas correspondentes ao 
-# módulo de perfil. Os métodos aqui são criados e chamados pela Factory (MainUnit.py) quando necessários. Eles são responsáveis pelo 
-# redirecionamento do usuário para páginas diferentes dependendo do tipo de usuário, edição de dados pessoais, visualização de informações
-# relativas aos cursos.
+# 	Este arquivo é responsável pelo armazenamento de todas as camadas 
+# correspondentes ao módulo de perfil. 
+#	Os métodos aqui são criados e chamados pela Factory (MainUnit.py)
+# quando necessários. Eles são responsáveis pelo redirecionamento do usuário
+# para páginas diferentes dependendo do tipo de usuário, edição de dados 
+# pessoais, visualização de informações relativas aos cursos.
 
 ## Interface para a camada de Apresentação de Usuário do módulo Profile.
-# É responsável pelo carregamento do template correto e processa os dados inseridos nos formulários de Perfil.
+# 	É responsável pelo carregamento do template correto e processa os 
+# 	dados inseridos nos formulários de Perfil.
 class IfUiProfile:
 	__metaclass__ = ABCMeta
 
@@ -35,18 +38,24 @@ class IfUiProfile:
 		if isinstance(value, IfBusProfile):
 			self.__bus = value
 		else:
-			raise TypeError("Expected IfBusProfile instance at UiProfile.__bus. Received " + str(type(value)) + " instead.")
+			raise TypeError("Expected IfBusProfile instance at \
+				UiProfile.__bus. Received " + 
+				str(type(value)) + " instead.")
 
 	@bus.deleter
 	def bus(self):
 		del self.__bus
 
-	## 'Run' é o principal método de qualquer classe de apresentação. Este método permite a Factory dar o controle do programa à este módulo.
+	## 'Run' é o principal método de qualquer classe de apresentação. 
+	# 	Este método permite a Factory dar o controle do programa 
+	# 	módulo.
 	@abstractmethod
 	def run(self, request): pass
 
 
-## Interface para a camada de Negócio do módulo de perfil. É responsável por 
+## Interface para a camada de Negócio do módulo de perfil.
+#	É responsável por executar a atualização do Cookie de dados do
+#	usuário, bem como a devida recuperação de dados para o sistema.
 class IfBusProfile:
 	__metaclass__ = ABCMeta
 
@@ -67,7 +76,9 @@ class IfBusProfile:
 		if isinstance(value, IfPersProfile):
 			self.__pers = value
 		else:
-			raise TypeError("Expected IfPersProfile instance at BusProfile.__pers. Received " + str(type(value)) + "instead.")
+			raise TypeError("Expected IfPersProfile instance at \
+				BusProfile.__pers. Received " + 
+				str(type(value)) + "instead.")
 
 	@pers.deleter
 	def pers(self):
@@ -77,12 +88,18 @@ class IfBusProfile:
 	def refreshUser(self, user): pass
 
 
+## Interface para a camada de Persistência do módulo de perfil.
+#	É responsável pela recuperação dos dados do usuário logado do banco
+#	de dados.
 class IfPersProfile:
 	__metaclass__ = ABCMeta
 
 	@abstractmethod
 	def fetch(self, user): pass
 
+## Camada de apresentação para estudantes.
+#	Deve carregar o devido template, contendo os dados básicos do estudante
+#	bem como os cursos matriculados e seu histórico.
 class UiProfileS(IfUiProfile): 
 
 	def run(self, request):
@@ -102,10 +119,12 @@ class PersProfileS(IfPersProfile):
 	def __select_field(self, uid, field):
 
 		try:
-			ret = Student.objects.get(identity=uid, field=field).value
+			ret = Student.objects.get(identity=uid, field=field)
+			ret = ret.value
 
 		except Student.MultipleObjectsReturned:
-			ret = map(lambda x: x.value, Student.objects.filter(identity=uid, field=field))
+			ret = map(lambda x: x.value, Student.objects.filter(
+					identity=uid, field=field))
 
 		except Student.DoesNotExist:
 			ret = None
@@ -115,7 +134,8 @@ class PersProfileS(IfPersProfile):
 	def fetch(self, username):
 
 		try:
-			uid = Student.objects.get(field='NAME', value=username).identity
+			uid = Student.objects.get(field='NAME', value=username)
+			uid = uid.identity
 
 			sf = lambda x: self.__select_field(uid, x)
 
