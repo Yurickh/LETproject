d3fea4056434b4d158da8aac9b7e26fac7e3ff1a
Yurick <yurick.hauschild@gmail.com>
2014-07-02 18:25:05 -0300

    Iniciado o desenvolvimento das forms remanescentes

--- ELO/ELO/static/Profile/script.js
+++ ELO/ELO/static/Profile/script.js
@@ -17,4 +17,25 @@ $(document).ready(function(){
 		$(".dialog").load("/assync/editfield/"+field);
 		$(".dialog").dialog("open")
 	});
+
+	//stylizes the interests text
+	/*$("#interests").on("input", function(){
+		$inter = $("#interests");
+		str = $.trim($inter.text());
+		if(str.slice(-1) == ',')
+		{
+			str = str.split(',');
+			strfin = "";
+
+			for (i in str)
+				if(str[i].slice(0, 26) != '<div class="interest_node">'
+					strfin += '<div class="interest_node">' + str[i] + '</div>';
+				else
+					strfin += str[i];
+
+			html = $.parseHTML( strfin );
+			$inter.empty();
+			$inter.append(html);
+		}
+	});*/
 });
--- ELO/Profile/forms.py
+++ ELO/Profile/forms.py
@@ -89,7 +89,7 @@ class BiosForm(forms.Form):
 #	Capaz de modificar a lista de interesses do jovem usuário.
 #	Talvez venhamos a utilizar isso mais para frente.
 class InterestsForm(forms.Form):
-	newdata		= forms.CharField(widget=forms.Textarea(attrs={'id':'interests'}))
+	newdata		= forms.CharField(widget=forms.Textarea(attrs={'id':'interests', 'style':'width:90%;'}))
 
 	def clean_newdata(self):
 		try:
