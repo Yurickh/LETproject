58d7236f8a67ef7adb71c8963343376fe564d564
Yurick <yurick.hauschild@gmail.com>
2014-08-14 15:42:58 -0300

    Corrigindo um erro que deixei para trás. Eu retirei a busca por interesses, mas esqueci de corrigir o javascript nesse sentido.

--- ELO/ELO/static/Profile/script_select2.js
+++ ELO/ELO/static/Profile/script_select2.js
@@ -1,10 +1,8 @@
 $(document).ready(function(){
 
 	var index;
-	var arr = $("#dlist").html().split(",");
 
 	$("#interests_form #id_newdata").select2({
-		tags: arr,
 		tokenSeparators: [","]
 	});
 
