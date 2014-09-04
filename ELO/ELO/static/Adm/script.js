9ffaa038b19281259a09620b937054b031a41999
Dayof <dayannefernandesc@gmail.com>
2014-09-04 14:48:38 -0300

    Criando arquivos suportes do Template do Adm

---
+++ ELO/ELO/static/Adm/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+	$(".dialog").dialog({
+		autoOpen: false,
+		hide: "slideUp",
+		modal: true,
+		resizable: false,
+		});
+
+	$("button[id^='edit_']").click(function(){
+		field = $(this).attr("id").slice(5);
+		fname = $(this).attr("title")
+
+		// Gets the title of the dialog from the title of button.
+		$(".dialog").dialog("option", "title", fname);
+
+		$(".dialog").load("/assync/editfield/"+field);
+		$(".dialog").dialog("open")
+	});
+
+});
