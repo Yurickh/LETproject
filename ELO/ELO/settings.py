13fac0029288f00f37f35734b643df3a74295d37
Dayof <dayannefernandesc@gmail.com>
2014-09-04 14:47:11 -0300

    Corrigindo filepath das mídias dos avatares

--- ELO/ELO/settings.py
+++ ELO/ELO/settings.py
@@ -59,7 +59,7 @@ LOCALE_PATHS = (
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = filepath('media/avatar')
+MEDIA_ROOT = filepath('media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
