fb039e3ca7bb8d22fe75515be421079be2ed3846
Yurick <yurick.hauschild@gmail.com>
2014-11-28 15:46:27 -0200

    Finalizada a organização do template Courses/frame.html. Além disso, foram modificados os seguintes itens: 1. Criada uma imagem baseada no camaleão da tela de login para o fundo do courses (media/images/cham_rel.png); 2. A imagem do camaleão da tela de login foi transferida para o servidor local, em vez de um servidor terceirizado (media/images/cham.png); 3. Corrigidos alguns detalhes de estilo do accordion feito no commit anterior; 4. Removida a pasta static/include. Utilizar a pasta static/vendor para isso; 5. Movidos os arquivos da pasta static/include para static/vendor; 6. Corrigido um erro no arquivo templates/includes/header.html, no qual o header engolia o content e o footer, caso o usuário estivesse logado; 7. Criado um arquivo static/Login/styles.css para evitar o hardcode de estilos no template, e para corrigir as falhas de organização geradas pela correção descrita no item 6; 8. Deletados arquivos de estilo _select2 do módulo de Perfil; 9. Retirado um tab desnecessário do arquivo templates/Profile/home.html.

--- ELO/ELO/static/Course/script.js
+++ ELO/ELO/static/Course/script.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
     
     $accordion = $("#module_accordion").accordion({
-        collapsible: true,
-        active: false,
         icons: {    "header": "ui-icon-plus",
                     "activeHeader": "ui-icon-minus"
                 },
-        heightStyle: 'content'
+        heightStyle: 'fill'
     });
+
+	
+
 });
