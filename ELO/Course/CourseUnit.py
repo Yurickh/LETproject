eaef9d99a98c06c075abf7191ea284d572dad6d8
Yurick <yurick.hauschild@gmail.com>
2014-12-20 01:56:34 -0200

    Adicionado documentação ao módulo de Curso.

--- ELO/Course/CourseUnit.py
+++ ELO/Course/CourseUnit.py
@@ -1,5 +1,17 @@
 #coding: utf-8
 
+## @file CourseUnit.py
+#   Este arquivo é responsável pelo armazenamento de todas as camadas 
+# correspondentes ao módulo de curso. 
+#   O módulo de curso é responsável por mostrar aos alunos as lições, bem como
+# administrar a resolução e correção de exercícios.
+#   Para tal, uma única janela é criada, e chamadas assíncronas são realizadas
+# para atualizar seu conteúdo, sendo elas chamadas de atualização de slides.
+#	Slides podem conter fragmentos de lições ou exercícios, depenendo de que
+# ponto da lição o usuário está navegando - ou do próprio módulo.
+#	O aluno só deve ser capaz de prosseguir no curso (lê-se ir para a próxima
+# lição/módulo) se tiver obtido uma pontuação mínima na lição/módulo anterior.
+
 from abc import*
 
 import ELO.locale.index as lang
@@ -13,12 +25,20 @@ from django.core.exceptions import PermissionDenied
 from django.http import Http404
 from django.forms import ValidationError
 
+##Macro responsável por armazenar a URL relativa dos templates de lições.
 LESSONS_URL = 'Course/lessons/'
+##Macro responsável por armazenar a URL relativa dos templates gerais do curso.
 GENERAL_URL = 'Course/general/'
 
+## Interface da camada de Apresentação do módulo de Curso.
+#	É responsável pelo devido processamento do frame de curso; seleção de
+# curso, módulo, lição e slide; formatação dos templates de exercícios; 
+# bem como o processamento do primeiro nível da administração dos dados
+# submetidos em exercícios.
 class IfUiCourse:
 	__metaclass__ = ABCMeta
 
+	## Força a criação da camada subjacente.
 	def __init__(self, bus):
 		try:
 			self.bus = bus
@@ -41,13 +61,28 @@ class IfUiCourse:
 	def bus(self):
 		del self.__bus
 
+	## run() é o principal método de qualquer classe de apresentação.
+	#	Este método permite à Factory (ver MainUnit.py) dar controle
+	#	do programa para este módulo.
+	#
+	#	@arg request	Objeto HttpRequest enviado pela requisição do navegador
+	#
+	#	@arg courseid	Identificador do curso.
+	#						Deve necessariamente ser um inteiro validado
+	#						através do objeto Id (vide BaseUnit.py).
 	@abstractmethod
 	def run(self, request, courseid=None): pass
 
 
+## Interface da camada de Negócio do módulo de Curso.
+#	É responsável pelo processamento inteligente dos dados, validação
+# e recuperação de dados.
+#	Esta camada é a interface necessária de comunicação entre a camada
+# de apresentação - o usuário - e a camada de persistência - o servidor.
 class IfBusCourse:
 	__metaclass__ = ABCMeta
 
+	## Garante a criação da camada subjacente.
 	def __init__(self, pers):
 		try:
 			self.pers = pers
@@ -75,18 +110,55 @@ class IfBusCourse:
 	#
 	#	@arg user		Objeto usuário, como no contido no cookie user.
 	#
-	#	@arg accesstype	String contendo "MODULE" ou "LESSON"
+	#	@arg accesstype	String contendo "MODULE", "LOCK" ou "LESSON"
+	#
+	#	@return 		Lista contendo todas os módulos cadastrados,
+	#						lições destravadas ou cadastrados para o
+	#						usuário 'user', variando sob o argumento
+	#						accesstype.
 	@abstractmethod
 	def getCompleted(self, user, accesstype): pass
 
+	## Método que recupera um objeto que representa um curso.
+	#
+	#	@arg user		Objeto usuário, como no contido no cookie user.
+	#
+	#	@arg courseid	Identificador do curso.
+	#						Deve ser um inteiro de acordo com o padrão de Id().
+	#
+	#	@return			Retorna um objeto curso com os seguintes atributos:
+	#						- NAME    : Identifica o nome do curso;
+	#						- ID      : Identifica univocamente o curso;
+	#						- STUDENTS: Lista de Ids de alunos cadastrados;
+	#						- MODULES : Lista de dicionários que representam
+	#										cada um dos módulos.
 	@abstractmethod
 	def getCourse(self, user, courseid): pass
 
+
+## Interface da camada de Persistência para o módulo de Curso.
+#	Deve ser capaz de acessar de forma transparente o banco de dados.
+#	Deve também ser a única forma de acessar tais dados dentro deste módulo.
 class IfPersCourse:
 
+	## Método que recupera o Id() de um objeto.
+	#
+	#	@arg field	Nome do campo a ser filtrado.
+	#
+	#	@arg value	Valor que deve haver no campo.
+	#
+	#	@arg db		Objeto do modelo a ser utilizado.
+	#					Deve ser importado de ELO.models.
 	@abstractmethod
 	def getid(self, field, value, db): pass
 
+	## Realiza uma pesquisa no banco de dados.
+	#
+	#	@arg id		Identificador do objeto requisitado.
+	#					Pode ser obtido através do método getid().
+	#	
+	#	@arg db		Objeto do modelo a ser utilizado.
+	#					Deve ser importado de ELO.models.
 	@abstractmethod
 	def fetch(self, id, db): pass
 
