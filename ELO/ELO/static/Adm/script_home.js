47109a0edf5ca1b085f63e671c3e1a5fa774b375
Yurick <yurick.hauschild@gmail.com>
2015-01-07 22:36:58 -0200

    Whitespace standard

--- ELO/ELO/static/Adm/script_home.js
+++ ELO/ELO/static/Adm/script_home.js
@@ -1,53 +1,53 @@
 $(document).ready(function(){
 
-	// Caracteristicas do Dialog.
-	$dialog = $(".dialog").dialog({
-		autoOpen: false,
-		show: {
-			effect: "blind",
-			duration: 1
-		},
-		hide: {
-			effect: "blind",
-			duration: 1
-		},
-		modal: true,
-		resizable: false,
-	});
+    // Caracteristicas do Dialog.
+    $dialog = $(".dialog").dialog({
+        autoOpen: false,
+        show: {
+            effect: "blind",
+            duration: 1
+        },
+        hide: {
+            effect: "blind",
+            duration: 1
+        },
+        modal: true,
+        resizable: false,
+    });
 
-	// Caracteristicas do Dialog.
-	$in_dialog = $(".inner_dialog").dialog({
-		autoOpen: false,
-		show: {
-			effect: "blind",
-			duration: 1
-		},
-		hide: {
-			effect: "blind",
-			duration: 1
-		},
-		modal: true,
-		resizable: false,
-	});
+    // Caracteristicas do Dialog.
+    $in_dialog = $(".inner_dialog").dialog({
+        autoOpen: false,
+        show: {
+            effect: "blind",
+            duration: 1
+        },
+        hide: {
+            effect: "blind",
+            duration: 1
+        },
+        modal: true,
+        resizable: false,
+    });
 
-	// Cria o evento de clicar e abrir os buttons da pagina de adm.
-	$("button").click(function(){		
+    // Cria o evento de clicar e abrir os buttons da pagina de adm.
+    $("button").click(function(){       
 
-		// Coleta o modelo sobre o qual a acao sera realizada, sendo eles
-		// estudante, professor ou curso.
-		model = $(this).attr("id").slice(4);
-		action = $(this).attr("id").slice(0,3);
-		// Coleta a acao que ira ser realizada, sendo elas de
-		// registro, edicao ou delecao.
-		text_button = $(this).text();
+        // Coleta o modelo sobre o qual a acao sera realizada, sendo eles
+        // estudante, professor ou curso.
+        model = $(this).attr("id").slice(4);
+        action = $(this).attr("id").slice(0,3);
+        // Coleta a acao que ira ser realizada, sendo elas de
+        // registro, edicao ou delecao.
+        text_button = $(this).text();
 
-		// Transforma o titulo do button como titulo do dialog.
-		$dialog.dialog("option", "title", text_button);
+        // Transforma o titulo do button como titulo do dialog.
+        $dialog.dialog("option", "title", text_button);
 
-		// Faz uma requisicao AJAX passando a acao e o modelo adequado.
-		$dialog.load("/assync/adm-edit/"+action+"/"+model+"/", function(){
-			$dialog.dialog('open');
-		});
-		
-	});
+        // Faz uma requisicao AJAX passando a acao e o modelo adequado.
+        $dialog.load("/assync/adm-edit/"+action+"/"+model+"/", function(){
+            $dialog.dialog('open');
+        });
+        
+    });
 });
--- ELO/ELO/static/Adm/script_info.js
+++ ELO/ELO/static/Adm/script_info.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
-	$("button[id^='edit_']").click(function(){
-		field = $(this).attr("id").slice(5);
-		fname = $(this).attr("title")
+    $("button[id^='edit_']").click(function(){
+        field = $(this).attr("id").slice(5);
+        fname = $(this).attr("title")
 
-		// Gets the title of the dialog from the title of button.
-		$(".dialog").dialog("option", "title", fname);
+        // Gets the title of the dialog from the title of button.
+        $(".dialog").dialog("option", "title", fname);
 
-		$(".dialog").load("/assync/edit-field/"+field+"/", function(){
-			$(".dialog").dialog("open");
-		});
-	});
+        $(".dialog").load("/assync/edit-field/"+field+"/", function(){
+            $(".dialog").dialog("open");
+        });
+    });
 });
