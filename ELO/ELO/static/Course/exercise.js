883b072c173c1f3168cd40f61fa3896bcece4b00
Yurick <yurick.hauschild@gmail.com>
2015-06-03 05:25:10 -0300

    Corrigindo padrão de espaçamento dos novos arquivos.

--- ELO/ELO/static/Course/exercise.js
+++ ELO/ELO/static/Course/exercise.js
@@ -1,12 +1,12 @@
 function loadExercise(arr)
 {
-	$a = $("#ans");
-	$a.load("assync/exercise",
-		{
-			'lesson_id': $info.id,
-			'slide_number': $info.slide,
-			'csrfmiddlewaretoken': serial[1].value
-		});
+    $a = $("#ans");
+    $a.load("assync/exercise",
+        {
+            'lesson_id': $info.id,
+            'slide_number': $info.slide,
+            'csrfmiddlewaretoken': serial[1].value
+        });
 }
 
 $("#exercise").submit(function(ev){
@@ -14,8 +14,8 @@ $("#exercise").submit(function(ev){
     serial = $(this).serializeArray();
 
 
-	// Se a pergunta foi respondida
-	if(serial[0].name == "options" || serial[0].name == "blank" || serial[0].name == "bloat")
-		loadExercise(serial);
+    // Se a pergunta foi respondida
+    if(serial[0].name == "options" || serial[0].name == "blank" || serial[0].name == "bloat")
+        loadExercise(serial);
     
 });
